583c2ed6428e5092b77c966030de4e4bc1c147fb
Catharina Mesquita <catha.ana.1994@gmail.com>
2020-06-12 11:53:19 -0300

    Adicionando o componente List

--- app/javascript/components/ListCustomer.js
+++ app/javascript/components/ListCustomer.js
@@ -1,13 +1,23 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Button, List } from "acmesquita-ui"
+
 class ListCustomer extends React.Component {
-  render () {
+
+  render() {
     console.log(this.props);
     const listItems = this.props.customers.map((customer) => <li key={customer.name}>{customer.name}</li>);
     return (
       <div>
         <strong>Clientes:</strong>
-        { listItems }
+        {listItems}
+        <Button onClick={() => { alert('hello') }}>Teste</Button>
+        <List
+          data={[
+            'João',
+            'Maria'
+          ]}
+        />
       </div>
     );
   }
